6ca7e3ec62c57890b6fb83079ef5139f1a0918e6
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-15 14:30:36 +0800

    add black组件相关

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Agree from '@/pages/agree'
+import Black from '@/pages/black'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/agree',
       name: 'Agree',
       component: Agree
+    },
+    {
+      path: '/black',
+      name: 'Black',
+      component: Black
     }
   ]
 })
