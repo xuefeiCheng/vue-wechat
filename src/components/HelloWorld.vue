037132fd991a58ff248f812619e45671e9348bc5
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-15 14:31:24 +0800

    helloworld --> home页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,121 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+<div>
+  <we-header :WeTitle="title" :isShow='false'></we-header>
+  <div class="test">
+    <div class="title">菜单选择</div>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+        <router-link to="/agree">免责声明</router-link>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
+        <router-link to="/black">黑名单管理</router-link>
       </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-    <div class="test">
-      测试
-    </div>
   </div>
+</div>
 </template>
 
 <script>
+import WeHeader from '@/components/Header'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: '首页'
     }
+  },
+  components: {
+    WeHeader
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 div.test {
-  height: 1rem;
-  background-color: #42b983;
+  height: 3rem;
+  padding: 4rem 0;
+  /* background-color: #42b983; */
   font-size: 12px;
+}
+div.title {
+  height: 1rem;
+  line-height: 1rem;
+  font-weight: bold;
+  padding-left: 0.4rem;
+}
+ul li{
+  height: 1rem;
+  line-height: 1rem;
+  padding-left: 0.4rem;
 }
 </style>
