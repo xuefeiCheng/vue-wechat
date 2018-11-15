819c6d9d6b2b411c782d15311f17721f2100cede
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-15 16:43:46 +0800

    引入mint-ui并测试比原有尺寸小

--- src/main.js
+++ src/main.js
@@ -3,14 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mint from 'mint-ui'
 import fastClick from 'fastclick'
 import 'lib-flexible/flexible.js'
 import 'babel-polyfill'
 import 'styles/iconfont.css'
 import 'styles/reset.css'
 import 'styles/border.css'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
+// 全局引用
+Vue.use(Mint)
 fastClick.attach(document.body)
 
 /* eslint-disable no-new */
