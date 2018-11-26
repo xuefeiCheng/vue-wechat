05660f924308d24601ff0761ec6d16a8fb091031
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-26 18:28:03 +0800

    拉黑原因 使用 假数据

--- src/pages/black/add.vue
+++ src/pages/black/add.vue
@@ -7,40 +7,11 @@
       </div>
       <div class="box-info">手机无需区号，添加后，黑名单中的号码来电会被直接拦截</div>
       <div class="box-middle">
-        <div class="coloumn">
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_swindle.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">诈骗电话</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_harass.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">恶意骚扰</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_medi.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">房产中介</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_ad.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">广告推销</div>
-          </div>
-        </div>
-        <div class="coloumn">
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_promote.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">保险理财</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_edu.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">教育培训</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_express.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">快递送餐</div>
-          </div>
-          <div class="reson-item">
-            <img class="reson-item-img" src="/static/img/black/addblack_other.png" alt="诈骗电话">
-            <div class="reson-item-img reson-item-title">其他</div>
+        <div class="coloumn" v-for="( img, index ) in imgList" :key="index">
+          <div class="reson-item" v-for="i in img" :key="i.value">
+            <img class="reson-item-img" :src="i.src[0]" alt="img.title" v-show='i.state'>
+            <img class="reson-item-img" :src="i.src[1]" alt="img.title" v-show='!i.state'>
+            <div class="reson-item-img reson-item-title">{{i.title}}</div>
           </div>
         </div>
       </div>
@@ -63,7 +34,57 @@ export default {
   },
   data () {
     return {
-      title: '新增黑名单'
+      title: '新增黑名单',
+      imgList:[[
+        {
+          value: '1',
+          state: false,
+          src: ['/static/img/black/addblack_swindle.png', '/static/img/black/addblack_swindle.png'],
+          title: '诈骗电话'
+        },
+        {
+          value: '2',
+          state: false,
+          src: ['/static/img/black/addblack_harass.png', '/static/img/black/addblack_harass.png'],
+          title: '恶意骚扰'
+        },
+        {
+          value: '3',
+          state: false,
+          src: ['/static/img/black/addblack_medi.png', '/static/img/black/addblack_medi.png'],
+          title: '房产中介'
+        },
+        {
+          value: '4',
+          state: false,
+          src: ['/static/img/black/addblack_ad.png', '/static/img/black/addblack_ad.png'],
+          title: '广告推销'
+        }],
+        [{
+          value: '5',
+          state: false,
+          src: ['/static/img/black/addblack_promote.png', '/static/img/black/addblack_promote.png'],
+          title: '保险理财'
+        },
+        {
+          value: '6',
+          state: false,
+          src: ['/static/img/black/addblack_edu.png', '/static/img/black/addblack_edu.png'],
+          title: '教育培训'
+        },
+        {
+          value: '7',
+          state: false,
+          src: ['/static/img/black/addblack_express.png', '/static/img/black/addblack_express.png'],
+          title: '快递送餐'
+        },
+        {
+          value: '8',
+          state: false,
+          src: ['/static/img/black/addblack_other.png', '/static/img/black/addblack_other.png'],
+          title: '其他'
+        }]
+      ]
     }
   },
   methods: {}
@@ -92,6 +113,8 @@ export default {
     display: flex
     flex-wrap: wrap
     align-content: center
+    .coloumn-basis
+      flex-basis: 100%
     .coloumn
       flex-basis: 100%
       display: flex
