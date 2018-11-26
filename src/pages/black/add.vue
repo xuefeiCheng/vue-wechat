d3fd21a5849efe13608a607b7f00c29824bfaea7
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-26 17:48:15 +0800

    拉黑原因 部分 布局

--- src/pages/black/add.vue
+++ src/pages/black/add.vue
@@ -8,16 +8,40 @@
       <div class="box-info">手机无需区号，添加后，黑名单中的号码来电会被直接拦截</div>
       <div class="box-middle">
         <div class="coloumn">
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_swindle.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">诈骗电话</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_harass.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">恶意骚扰</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_medi.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">房产中介</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_ad.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">广告推销</div>
+          </div>
         </div>
         <div class="coloumn">
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
-          <div class="reson-item">1</div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_promote.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">保险理财</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_edu.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">教育培训</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_express.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">快递送餐</div>
+          </div>
+          <div class="reson-item">
+            <img class="reson-item-img" src="/static/img/black/addblack_other.png" alt="诈骗电话">
+            <div class="reson-item-img reson-item-title">其他</div>
+          </div>
         </div>
       </div>
       <div class="box-bottom">
@@ -75,7 +99,8 @@ export default {
       .reson-item
         width 2.4rem
         height 2.4rem
-        border 1px solid red
-        background #cccccc
-
+        .reson-item-img
+          width 60%
+          padding 0 20%
+          text-align center
 </style>
