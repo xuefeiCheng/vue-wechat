a6105e298ca3cc10b14d6f18217fa55cfc94cef9
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-28 19:06:13 +0800

    新增拦截分类设置 界面 完成上半部分样式

--- src/pages/classify/list.vue
+++ src/pages/classify/list.vue
@@ -7,18 +7,17 @@
           <i class="iconfont icon-light font-color-FFF"></i>
           <span>开启的分类来电时将为您拦截！点击分类修改标记次数！</span>
         </div>
-        <div class="top-row input-group">
-          <input class="qh form-control" placeholder="区号"/>
-          <input class="phone form-control border-noRedius-r" ref="phone" placeholder="手机或固话号" />
-          <div class='lxr border-radius-12 border-noRedius-l'><i class="iconfont icon-lianxiren font-size-30 font-color-FFF"></i></div>
+        <div class="top-row">
+          <div class="row-left">
+            <div class="classifyIcon"></div>
+          </div>
+          <div class='row-right'>
+            <div class="classifyCon font-color-FFF font-size-17">
+              <div class="con-top">骚扰拦截已开启</div>
+              <div class="con-top con-top-button">拦截关闭</div>
+            </div>
+          </div>
         </div>
-        <div class="top-row input-group">
-          <input class="all form-control" placeholder="请填写备注（6字以内）"/>
-        </div>
-      </div>
-      <div class="box-bottom">
-        <x-button type="primary" class="globalBtn" @click.native="setFoucs">立即添加</x-button>
-        <x-button type="default" class="globalBtn" link="/white/list">白名单列表</x-button>
       </div>
     </div>
   </div>
@@ -36,12 +35,6 @@ export default {
     return {
       title: '拦截分类设置'
     }
-  },
-  methods: {
-    setFoucs () {
-      // 若没有数据 设置焦点到 手机号或区号
-      this.$refs.phone.focus()
-    }
   }
 }
 </script>
@@ -58,26 +51,40 @@ export default {
     .top-row
       box-sizing border-box
       display flex
-      height 2.2rem
+      height 4rem
+      line-height 4rem
       justify-content center
-      padding-top 1rem
-      .qh
-        width 1.5rem
-        margin-right 0.2667rem
-      .phone
-        width 5rem
-      .lxr
-        background #23d4ea
-        border-left: 1px solid #fff
-        line-height 1.2rem
-        width 1rem
-      .all
-        width 8.5rem
+      padding-top 10px
+      .row-left
+        flex 0 0 20%
+        .classifyIcon
+          background url('/static/img/classify/classify_icon_off.png') no-repeat center center
+          background-size auto 70%
+          display block
+          width 3rem
+          height 3rem
+          margin: 0.5rem auto
+      .row-right
+        flex 0 0 60%
+        .classifyCon
+          height 3rem
+          margin: 0.5rem auto
+          .con-top
+            height 1rem
+            width 4rem
+            margin auto
+            line-height 1rem
+            text-align center
+            margin-top .5rem
+          .con-top-button
+            margin-top .5rem
+            background #c71010
+            border 1px solid #c71010
+            border-radius 2px
   .box-info
     height 0.7467rem
     line-height 0.72rem
     background #23d4ea
-    padding-left 0.1333rem
     color #fff
     text-align center
 </style>
