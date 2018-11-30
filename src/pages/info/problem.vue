38a38008680f1ad7c8209858ae7e6d793239a3ca
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-30 11:34:17 +0800

    常见问题 start

---
+++ src/pages/info/problem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <we-header :WeTitle="title"></we-header>
+  </div>
+</template>
+
+<script>
+import WeHeader from '@/components/Header'
+export default {
+  components: {
+    WeHeader
+  },
+  data () {
+    return {
+      title: '常见问题'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus"></style>
