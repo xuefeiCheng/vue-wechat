14c57f42ffa07f038e14701230d4eab8cdd561c5
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-30 10:16:44 +0800

    feedback start

---
+++ src/pages/info/feedback.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <we-header :WeTitle="title"></we-header>
+  </div>
+</template>
+
+<script>
+import WeHeader from '@/components/Header'
+export default {
+  components: {
+    WeHeader
+  },
+  data () {
+    return {
+      title: '意见反馈'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus"></style>
