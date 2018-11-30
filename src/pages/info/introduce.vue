747f265ca31e8250b0c273ba1c100644af9c284c
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-30 15:55:11 +0800

    introduce end

--- src/pages/info/introduce.vue
+++ src/pages/info/introduce.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
     <we-header :WeTitle="title"></we-header>
+    <div class="padding10 padding-noBottom">
+      <span class="strong">一个动作终结电话骚扰</span>
+      <p>
+        最近经常接到奇怪的电话<br/>
+        不是叫我买房<br/>
+        就是问我办不办贷款<br/>
+        骚扰电话响不停，让人心烦到不行<br/><br/>
+        在我们的日常生活中<br/>
+        各种营销、各种广告<br/>
+        真是让人防不胜防，痛恨至极<br/>
+        仔细探究<br/>
+        这些骚扰电话是如何知道我们号码的呢？<br/>
+        <span  class="strong colorRed">应该如何防骚扰？</span><br/>
+        别担心！终极处理办法现在送上！<br/><br/>
+        <span class="strong">“绿盾”防护业务</span><br/>
+        <img alt="绿盾防护" src="/static/img/info/logo_zj.jpg" style="width: 100%;">
+        开通“绿盾”防护业务<br/>
+        可以在呼叫达到用户手机前<br/>
+        <span class="strong colorRed">拦截骚扰呼叫，放行正常呼叫</span><br/><br/>
+        <span class="strong">业务说明</span><br/>
+        “绿盾”防护是一款采用<span class="colorGreen">云端拦截</span>技术的<span class="colorGreen">免费产品</span>，
+        为用户提供专业的骚扰、诈骗电话拦截服务，<span class="colorGreen">无需安装app</span>就能防骚扰，特省心！
+      </p>
+    </div>
   </div>
 </template>
 
