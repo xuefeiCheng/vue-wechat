a1676c643c448ac8325786c8979a4a2a2b8b3241
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-30 15:05:57 +0800

    绿盾介绍开始

---
+++ src/pages/info/introduce.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <we-header :WeTitle="title"></we-header>
+  </div>
+</template>
+
+<script>
+import WeHeader from '@/components/Header'
+export default {
+  components: {
+    WeHeader
+  },
+  data () {
+    return {
+      title: '绿盾介绍'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus"></style>
