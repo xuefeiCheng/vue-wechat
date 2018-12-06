cb423676cd9a77825e6db23243c6f5be53eafe3f
xuefeicheng <xuefeicheng_ytu@163.com>
2018-12-06 13:18:21 +0800

    layout组件 文件变更位置

---
+++ src/components/layout/layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <we-header :WeTitle="title" :leftOptions="leftOptions"></we-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import WeHeader from '@/components/Header'
+export default {
+  components: {
+    WeHeader
+  },
+  mounted () {
+    this.title = this.$router.history.current.name || '标题'
+  },
+  data () {
+    return {
+      title: '',
+      leftOptions: {
+        showBack: false
+      }
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.title = this.$router.history.current.name || '标题'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
