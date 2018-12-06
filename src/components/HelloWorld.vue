a92fd05cac5035c47cae2461f19aba7027a79792
xuefeicheng <xuefeicheng_ytu@163.com>
2018-12-06 10:48:56 +0800

    首页目录结构变更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,41 +3,29 @@
   <we-header :WeTitle="title" :isShow='false'></we-header>
   <div class="test">
     <div class="title">菜单选择</div>
-      <ul>
-        <li>
-          <router-link to="/agree">免责声明</router-link>
-        </li>
-        <li>
-          <router-link to="/center">个人中心</router-link>
-        </li>
-        <li>
-          <router-link to="/feedback/suggest">意见反馈</router-link>
-        </li>
-        <li>
-          <router-link to="/introduce">绿盾介绍</router-link>
-        </li>
-        <li>
-          <router-link to="/problem">常见问题</router-link>
-        </li>
-        <li>
-          <router-link to="/test01">ListView测试</router-link>
-        </li>
-        <li>
-          <router-link to="/test02">LayOut测试</router-link>
-        </li>
-        <li>
-          <router-link to="/black/list">黑名单管理</router-link>
-        </li>
-        <li>
-          <router-link to="/white/list">白名单管理</router-link>
-        </li>
-        <li>
-          <router-link to="/classify/list">拦截分类设置</router-link>
-        </li>
-        <li>
-          <router-link to="/classify/search">拦截记录查询</router-link>
-        </li>
-      </ul>
+      <div>
+        <ul><li class="padding-left0">我的绿盾</li> </ul>
+        <ul>
+          <li><router-link to="/center">个人中心</router-link></li>
+          <li><router-link to="/introduce">绿盾介绍</router-link></li>
+          <li><router-link to="/feedback/suggest">意见反馈</router-link></li>
+          <li><router-link to="/problem">常见问题</router-link></li>
+        </ul>
+      </div>
+      <div>
+        <ul><li class="padding-left0">业务设置</li> </ul>
+        <ul>
+          <li><router-link to="/black/list">黑名单管理</router-link></li>
+          <li><router-link to="/white/list">白名单管理</router-link></li>
+          <li><router-link to="/classify/list">拦截分类设置</router-link></li>
+        </ul>
+      </div>
+      <div>
+        <ul><li class="padding-left0">记录查询</li> </ul>
+        <ul>
+          <li><router-link to="/classify/search">拦截记录查询</router-link></li>
+        </ul>
+      </div>
   </div>
 </div>
 </template>
@@ -70,9 +58,15 @@ div.title {
   font-weight: bold;
   padding-left: 0.4rem;
 }
+ul {
+  padding-left: 0.4rem;
+}
 ul li{
   height: 1rem;
   line-height: 1rem;
   padding-left: 0.4rem;
+}
+.padding-left0 {
+  padding-left: 0;
 }
 </style>
