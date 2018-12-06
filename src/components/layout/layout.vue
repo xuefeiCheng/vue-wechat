942db60c0f18dada2b44bf8e6ba993be2bc05704
xuefeicheng <xuefeicheng_ytu@163.com>
2018-12-06 14:05:14 +0800

    layout组件 中引入header组件，控制左侧返回按钮显示，阻止返回按钮默认事件，添加自定义事件，返回首页；并且根据路由变化，首页不展示返回按钮

--- src/components/layout/layout.vue
+++ src/components/layout/layout.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <we-header :WeTitle="title" :leftOptions="leftOptions"></we-header>
+    <we-header
+    :WeTitle="title"
+    :leftOptions="leftOptions"
+    :onClickBack="backToHome"></we-header>
     <router-view/>
   </div>
 </template>
@@ -13,18 +16,27 @@ export default {
   },
   mounted () {
     this.title = this.$router.history.current.name || '标题'
+    this.leftOptions.showBack = (this.$router.history.current.path !== '/menu')
   },
   data () {
     return {
       title: '',
       leftOptions: {
-        showBack: false
+        showBack: true,
+        preventGoBack: true,
+        backText: '返回首页'
       }
+    }
+  },
+  methods: {
+    backToHome () {
+      this.$router.push('/')
     }
   },
   watch: {
     '$route' (to, from) {
       this.title = this.$router.history.current.name || '标题'
+      this.leftOptions.showBack = (to.path !== '/menu')
     }
   }
 }
